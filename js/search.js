6b1046054e3b76ed1f5b69de8082f5baa9aefcfe
TrenTake <stephanie_bobyr10@mail.ru>
2020-06-12 22:38:33 +0300

    Завершение

---
+++ js/search.js
@@ -0,0 +1,5 @@
+var searchLink = document.querySelector(".search-hotel");
+var searchForm = document.querySelector(".form-search")
+searchLink.addEventListener("click", function(evt) {
+  evt.preventDefault();
+});
